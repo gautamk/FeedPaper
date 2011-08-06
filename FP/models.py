6da32b509a563276debb510c62f8bb90e01c3f8e
Gautam Kumar <gautham5678@gmail.com>
2011-08-06 09:49:53 +0530

    Checksum attribute added to itemDB to prevent duplication of items

--- FP/models.py
+++ FP/models.py
@@ -14,5 +14,14 @@ class itemDB(models.Model):
     description = models.TextField()
     link_url = models.URLField()
     time_stamp = models.DateTimeField(auto_now = True)
+    
+    # Check sum is used to prevent duplication of entries 
+    checksum = models.CharField(max_length = 200 , unique = True , null = False)
+    
+    
+    # Add a checksum and add a unique Index to it 
+
+        
+    
     def __unicode__(self):
         return self.title
--- FP/views.py
+++ FP/views.py
@@ -1,20 +1,32 @@
 # Create your views here
+import feedparser
+import hashlib
+from django.db import IntegrityError
 from FeedPaper.FP.models import *
 from django.http import HttpResponse ,HttpResponseRedirect
-import feedparser
 from django.template import loader , Context
+
 
 def UpdateItems(request):
     feeds = feedDB.objects.all()
     for feed in feeds:
         parse = feedparser.parse(feed.url)
         for entry in parse['entries']:
-            itemDB(
-                   title =  entry.title,
-                   description = entry.summary,
-                   link_url = entry.link ,
-                   feed = feed,
-                   ).save()
+            str = entry.title + entry.summary + entry.link # Prepare the strings for Hashing
+            str = str.encode('ascii' , 'xmlcharrefreplace')# Convert Unicode to ascii and replace
+            # non ascii characters with xml reference characters
+            hash = hashlib.sha1(str).hexdigest().encode('utf-8') # Compute sha-1 hash and convert to Unicode
+            try: 
+                itemDB(
+                       title =  entry.title,
+                       description = entry.summary,
+                       link_url = entry.link ,
+                       feed = feed,
+                       checksum = hash ,
+                       ).save()
+            except IntegrityError:
+                pass
+                
     
     return HttpResponseRedirect('/admin')
 
@@ -29,6 +41,9 @@ def ShowUpdateItems(request):
         <h2><a href="%s" target="_blank">Feed Link</a></h2>
         """ %(feed.keyword , feed.url)
         for entry in parse['entries']:
+            str = entry.title + entry.summary + entry.link
+            str = str.encode('ascii' , 'xmlcharrefreplace')
+            hash = hashlib.sha1(str).hexdigest().encode('utf-8')
             output += """
             <style>
             div
@@ -40,9 +55,9 @@ def ShowUpdateItems(request):
             <div>
             Title : <p><a href="%s" target="_blank"> %s </a></p>
             Summary : <p> %s </p>
-            
+            Hash: <p> %s </p>
             </div>
-            """%(entry.link  , entry.title , entry.summary , )
+            """%(entry.link  , entry.title , entry.summary , hash )
     
     return HttpResponse(output)
     
